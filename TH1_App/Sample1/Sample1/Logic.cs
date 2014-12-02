4c4553c2b2e526ef8efbb84045da7e1cf296ae59
waruimicky <yamano.tomonori@gmail.com>
2014-12-02 14:16:22 +0900

    logicクラスの追加

---
+++ TH1_App/Sample1/Sample1/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample1
+{
+    //画面とロジックを分割するためのクラス
+    class Logic
+    {
+        //sample add
+        public static int add(int x, int y)
+        {
+            //呼び出し側使用例：
+            //Logic.add(10, 20);
+            return x + y;
+        }
+    }
+}
